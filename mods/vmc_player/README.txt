a4e26189f05051efaddf775a7c2366062d2fafa2
ROllerozxa <rollerozxa@voxelmanip.se>
2023-03-13 22:02:59 +0100

    Rewrite player_api code
    Hopefully clean room enough to become unencumbered

--- mods/vmc_player/README.txt
+++ mods/vmc_player/README.txt
@@ -1,13 +1,7 @@
 
-Authors of source code
-----------------------
-Originally by celeron55, Perttu Ahola <celeron55@gmail.com> (LGPLv2.1+)
-Various Minetest developers and contributors (LGPLv2.1+)
-
 Authors of media (textures, models and sounds)
 ----------------------------------------------
 Original model by MirceaKitsune (CC BY-SA 3.0).
 Various alterations and fixes by kilbith, sofar, xunto, Rogier-5, TeTpaAka, Desour,
 stujones11, An0n3m0us (CC BY-SA 3.0):
   character.b3d
-  character.blend
--- mods/vmc_player/init.lua
+++ mods/vmc_player/init.lua
@@ -1,42 +1,77 @@
--- player/init.lua
 
-dofile(minetest.get_modpath("vmc_player") .. "/api.lua")
+local animations = {
+	-- Standard animations.
+	stand     = {x = 0,   y = 79},
+	lay       = {x = 162, y = 166},
+	walk      = {x = 168, y = 187},
+	mine      = {x = 189, y = 198},
+	walk_mine = {x = 200, y = 219},
+	sit       = {x = 81,  y = 160},
+}
 
--- Default player appearance
-player_api.register_model("character.b3d", {
-	animation_speed = 30,
-	textures = {"character.png"},
-	animations = {
-		-- Standard animations.
-		stand     = {x = 0,   y = 79},
-		lay       = {x = 162, y = 166},
-		walk      = {x = 168, y = 187},
-		mine      = {x = 189, y = 198},
-		walk_mine = {x = 200, y = 219},
-		sit       = {x = 81,  y = 160},
-	},
-	collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
-	stepheight = 1.1,
-	eye_height = 1.47,
-})
+local player_anim = {}
+local player_sneak = {}
 
--- Update appearance when the player joins
 minetest.register_on_joinplayer(function(player)
-	player_api.player_attached[player:get_player_name()] = false
-	player_api.set_model(player, "character.b3d")
+	player:set_properties{
+		mesh = "character.b3d",
+		textures = {"character.png"},
+		visual = "mesh",
+		visual_size = {x = 1, y = 1},
+		collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
+		stepheight = 0.6,
+		eye_height = 1.47,
+	}
+
 	player:set_local_animation(
-		{x = 0,   y = 79},
-		{x = 168, y = 187},
-		{x = 189, y = 198},
-		{x = 200, y = 219},
-		30
-	)
+		animations.stand,
+		animations.walk,
+		animations.mine,
+		animations.walk_mine,
+		30)
 
 	player:override_day_night_ratio(1)
 
 	local name = player:get_player_name():lower()
 	local skin = io.open(minetest.get_modpath("vmc_player").."/textures/character_"..name..".png")
 	if skin ~= nil then
-		player_api.set_textures(player, {"character_"..name..".png"})
+		player:set_properties{ textures = {"character_"..name..".png"} }
+	end
+end)
+
+local function set_animation(player, anim_name, speed)
+	local name = player:get_player_name()
+	if player_anim[name] == anim_name then return end
+
+	player_anim[name] = anim_name
+	player:set_animation(animations[anim_name], speed)
+end
+
+minetest.register_globalstep(function()
+	for _, player in ipairs(minetest.get_connected_players()) do
+		local name = player:get_player_name()
+		local controls = player:get_player_control()
+
+		local anim_speed = controls.sneak and 15 or 30
+		local anim_name
+
+		if controls.up or controls.down or controls.left or controls.right then
+			if player_sneak[name] ~= controls.sneak then
+				player_anim[name] = nil
+				player_sneak[name] = controls.sneak
+			end
+
+			if controls.LMB or controls.RMB then
+				anim_name = "walk_mine"
+			else
+				anim_name = "walk"
+			end
+		elseif controls.LMB or controls.RMB then
+			anim_name = "mine"
+		else
+			anim_name = "stand"
+		end
+
+		set_animation(player, anim_name, anim_speed)
 	end
 end)
