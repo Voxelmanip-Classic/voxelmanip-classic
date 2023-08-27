079285309d703fdf2464e18f8d78e1320961285b
ROllerozxa <rollerozxa@voxelmanip.se>
2023-08-27 22:39:15 +0200

    Replace flyspeed with just a basic speed set command

--- mods/vmc_cmds/init.lua
+++ mods/vmc_cmds/init.lua
@@ -2,3 +2,16 @@
 include('build')
 include('fun')
 include('msgblock')
+
+minetest.register_chatcommand('speed', {
+	params = "<speed>",
+	description = "Change your speed.",
+	privs = { fast = true },
+
+	func = function(name, param)
+		local speed = tonumber(param) or -1
+		if speed >= 0 and speed < 10 then
+			minetest.get_player_by_name(name):set_physics_override{ speed = speed }
+		end
+	end,
+})
